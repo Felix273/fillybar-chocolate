4c12387f8a2d0e622eac438083dfb218fbaabda7
Felix Ngitari <eregaefelix8@gmail.com>
2023-11-21 12:46:28 +0300

    fillybar chocolate

---
+++ src/api/itemnew/controllers/itemnew.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * itemnew controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::itemnew.itemnew');
---
+++ src/api/itemnew/routes/itemnew.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * itemnew router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::itemnew.itemnew');
